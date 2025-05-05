0c00495f795f93de8530ccce1b016cf0057e3920
WasilewskiMarcin <marcin.wasilewski0499@gmail.com>
2025-05-06 00:01:11 +0200

    navigation.navigate not working...

--- Teamfy/app/Root.tsx
+++ Teamfy/app/Root.tsx
@@ -5,18 +5,13 @@ import { useAuth } from '@clerk/clerk-expo'
 import LoginScreen from './(auth)/LoginScreen'
 import UpcomingScreen from './(tabs)/UpcomingScreen'
 import SettingsScreen from './(tabs)/Settings/SettingsScreen'
-import { View ,StyleSheet, Text} from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { View } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import ProjectManagerScreen from './(tabs)/ProjectManagerScreen'
 import {Ionicons} from '@expo/vector-icons'
 import CalendarIconWithDate from '@/components/CalendarIconWithDate'
-
-
-import { SignOutButton } from '@/components/SignOutButton'
-import { responsiveWidth, responsiveHeight, responsiveFontSize } from 'react-native-responsive-dimensions'
-import ProfileButton from '@/components/ProfileButton'
+import TopNavigator from '@/components/TopNavigator'
+import CreateEventScreen from './(tabs)/CreateEventScreen'
 
 const Tab = createBottomTabNavigator()
 export default function RootLayout() {
@@ -27,32 +22,31 @@ export default function RootLayout() {
 					<SafeUserProvider>
 						<TopNavigator/>
 						<RootNavigation />
-						{/* <Stack screenOptions={{ headerShown: false }} /> */}
 					</SafeUserProvider>
 				</SafeAreaView>
 			</SafeAreaProvider>
 		</ClerkAndConvexProvider>
 	)
 }
-type RootStackParamList = {
-	Main: undefined
-	Settings: undefined
-}
 
-type NavigationProp = NativeStackNavigationProp<RootStackParamList>
+
 function RootNavigation() {
 	const { isSignedIn } = useAuth()
-	const navigation = useNavigation<NavigationProp>()
 	if (isSignedIn) {
 		return (
 			<View style={{ flex: 1 }}>
 				<Tab.Navigator screenOptions={{ headerShown: false }}>
-					<Tab.Screen  name='UpcomingScreen' component={UpcomingScreen} options={{
+					<Tab.Screen  name='Upcoming' component={UpcomingScreen} options={{
 						tabBarIcon: ({size, color}) => <CalendarIconWithDate size={size} color={color} />,
 						}}/>
-					<Tab.Screen name='ProjectManagerScreen' component={ProjectManagerScreen} options={{
+					<Tab.Screen name='ProjectManager' component={ProjectManagerScreen} options={{
 						tabBarIcon: ({ size, color }) => (
 							<Ionicons name="home" size={size} color={color} />
+						  ),
+						}}/>
+						<Tab.Screen name='CreateEvent' component={CreateEventScreen} options={{
+						tabBarIcon: ({ size, color }) => (
+							<Ionicons name="add-circle" size={size} color={color} />
 						  ),
 						}}/>
 
@@ -68,50 +62,3 @@ function RootNavigation() {
 		return <LoginScreen />
 	}
 }
-function TopNavigator() {
-	return (
-		<View>
-			<View style={styles.header}>
-				<View style={styles.profileButton}>
-					<ProfileButton />
-				</View>
-				<Text style={styles.logoText}>Teamfy</Text>
-				<View style={styles.logoutWrapper}>
-					<SignOutButton />
-				</View>
-			</View>
-
-			<View style={styles.content}></View>
-		</View>
-	)
-}
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
-	header: {
-		padding: responsiveWidth(3.5),
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-		position: 'relative',
-	},
-	logoText: {
-		fontSize: responsiveFontSize(3.5),
-		fontWeight: 'bold',
-	},
-	logoutWrapper: {
-		position: 'absolute',
-		right: responsiveWidth(4),
-		top: responsiveHeight(3),
-	},
-	profileButton: {
-		position: 'absolute',
-		left: responsiveWidth(4),
-		top: responsiveHeight(3),
-	},
-	content: {
-		flex: 1,
-	},
-})
